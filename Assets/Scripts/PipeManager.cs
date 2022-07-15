5532fea20a995b319ed2983348c84cf2e3685083
AmirBana <83809960+AmirBana@users.noreply.github.com>
2022-07-16 02:32:41 +0430

    Pipelines Rotation

--- Assets/Scripts/PipeManager.cs
+++ Assets/Scripts/PipeManager.cs
@@ -5,21 +5,31 @@ using UnityEngine;
 
 public class PipeManager : MonoBehaviour
 {
-    private CapsuleCollider collider;
+    private Rigidbody rb;
     public Action shouldGenerate;
+    public float tarqueSpeed = 1f;
     // Start is called before the first frame update
     void Start()
     {
-        collider = GetComponent<CapsuleCollider>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(CompareTag("Left"))
+        {
+            rb.angularVelocity = new Vector3(0f, 0f, -1f * tarqueSpeed);
+        }
+        else if(CompareTag("Right"))
+        {
+            rb.angularVelocity = new Vector3(0f, 0f, 1f * tarqueSpeed);
+            // rb.AddTorque(0f,0f, 10f * tarqueSpeed, ForceMode.Impulse);
+        }
     }
     private void OnCollisionEnter(Collision collision)
     {
-        if(collider.gameObject.CompareTag("Player"))
+        if(collision.gameObject.CompareTag("Player"))
         {
             if (shouldGenerate != null)
             {
